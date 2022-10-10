42fb3d998c2618f3330e49fcc890a517497c9375
Lupe <lupemaydana@gmail.com>
2022-10-10 13:29:07 -0500

    remove duplicated file

--- tests/example2.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('test', async ({ page }) => {
-  await page.goto('https://codepen.io/');
-  await page.locator('nav[role="navigation"]:has-text("Try Our Online EditorStart CodingSearch PensChallengesSparkCodePen Your new deve")').getByRole('link', { name: 'Challenges' }).click();
-  await expect(page).toHaveURL('https://codepen.io/challenges');
-});
