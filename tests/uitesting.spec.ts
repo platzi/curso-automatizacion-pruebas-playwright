3f08855437d5735e5e3eec5f0a714acc4c995064
Lupe <lupemaydana@gmail.com>
2022-10-11 12:16:42 -0500

    clase de selectores

--- tests/uitesting.spec.ts
+++ tests/uitesting.spec.ts
@@ -6,9 +6,11 @@ test('test', async ({ page }) => {
 
   await page.goto('http://uitestingplayground.com/');
 
-  await page.getByText('Aristte').click();
+  // await page.getByText('Aristotle').click();
+  await page.locator('text=Aristotle').click();
 
-  await page.getByRole('link', { name: 'Resources' }).click();
+  // await page.getByRole('link', { name: 'Resources' }).click();
+  await page.locator('a.nav-link:has-text("Resources")').click();
   await expect(page).toHaveURL('http://uitestingplayground.com/resources');
 
   await page.getByRole('link', { name: 'Home' }).click();
